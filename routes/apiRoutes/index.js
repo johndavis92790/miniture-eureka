31fac129d0109da470cbc0c3a6f9cfb4f973a623
johndavis <johndavis92790@gmail.com>
2022-01-16 16:05:36 -0700

    got get, post and delete routes setup and working, created heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,42 +1,56 @@
-
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
 const uniqid = require('uniqid'); 
 
-
-
-const notes = require('../../db/db');
+var { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
   res.json(notes);
 });
 
-router.get('/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+router.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  notes = notes.filter(function(note) { return note.id != id }); 
+
+  console.log(notes);
+
+  fs.writeFileSync(
+    path.join(__dirname, '../../db/db.json'),
+    JSON.stringify({ notes }, null, 2)
+  );
+
+  res.json(notes);
+
+  // if (result) {
+  //   res.json(result);
+  // } else {
+  //   res.send(404);
+  // }
 });
 
 router.post('/notes', (req, res) => {
-  // const resJson = req.body;
-  console.log('req', req.body);
-  // console.log('res', res);
   const id = uniqid();
-  console.log(id);
+  const title = req.body.title;
+  const text = req.body.text;
 
-  // JSON.stringify({ id }, null, 0)
+  let objText = 
+  `{ 
+      "id": "` + id + `", 
+      "title": "` + title + `", 
+      "text": "` + text + `" 
+    }`;
 
-  var postNote = req.body;
-  postNote.push(id);
+  const obj = JSON.parse(objText);
+  notes.push(obj);
 
-  console.log(postNote);
-  // const newNote = createNewNote(req.body, notes);
+  fs.writeFileSync(
+    path.join(__dirname, '../../db/db.json'),
+    JSON.stringify({ notes }, null, 2)
+  );
 
-  
-  res.json(postNote);
-  
+  res.json(notes);
 });
 
 module.exports = router;
